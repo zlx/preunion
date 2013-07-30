21f0a9047d397df26a493fe6b89c6805ca42401d
soffolk <zlx.star@gmail.com>
2013-07-30 21:12:27 +0800

    make ranking sortable

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,5 +1,16 @@
 class RankingController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
   def index
-    @users = User.order("score desc").page(params[:page]).per(20)
+    @users = User.order(sort_column + " " + sort_direction).page(params[:page]).per(20)
+  end
+
+  private
+  def sort_column
+    User.column_names.include?(params[:sort]) ? params[:sort] : "score"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "desc"
   end
 end
