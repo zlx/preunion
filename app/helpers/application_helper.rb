30f0615dd94660f2ad57bf7233026a076f0efe76
soffolk <zlx.star@gmail.com>
2013-07-30 21:23:39 +0800

    link to github url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -34,4 +34,8 @@ module ApplicationHelper
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}
   end
+
+  def github_url(name)
+    "https://github.com/bay6/" + name
+  end
 end
